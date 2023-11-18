c0dd1e1da834669cda24343be814b02b567ccb41
RadioActiveBlackTi <dorae0520@naver.com>
2023-11-18 23:57:21 +0900

    file renamed

--- running_example.py
+++ running_example.py
@@ -1,6 +1,6 @@
 import torch
 import model
-import data_collect
+import scanner
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -10,9 +10,9 @@ if __name__=="__main__":
     net.to(device)
     net.eval()
 
-    modis = data_collect.modi_connect()
+    modis = scanner.modi_connect()
     input("Continue to type: ")
-    x = data_collect.scan_key(10, modis)
+    x = scanner.scan_key(10, modis)
     input("Continue to type: ")
-    y = data_collect.scan_key(10, modis)
+    y = scanner.scan_key(10, modis)
     print(net(torch.FloatTensor(x).unsqueeze(0), torch.FloatTensor(y).unsqueeze(0)))
